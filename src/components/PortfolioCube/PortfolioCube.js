f414ecdb8bc63d4cd92f9824e58fb4e889e5217e
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2021-06-03 16:13:03 -0700

    begin setting up portfolioCube for website display

---
+++ src/components/PortfolioCube/PortfolioCube.js
@@ -0,0 +1,12 @@
+
+import classes from './PortfolioCube.module.css';
+
+function PortfolioCube() {
+    return (
+        <div className={[classes.PortfolioCube, 'font_limelight'].join(' ')}>
+            <h1>Coming Soon...</h1>
+        </div>
+    );
+}
+
+export default PortfolioCube;
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,19 @@
+
+import classes from './Layout.module.css';
+
+
+function Layout(props) {
+
+   
+    return(
+        <div className={classes.Wrapper}>
+            
+            <div className={classes.Layout}>
+                {props.children}
+            </div>
+        </div>
+    );
+}
+
+export default Layout;
+
