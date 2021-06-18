7c36473d2f3ca2d7f436a402d376eea21a051c03
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2021-06-18 02:11:06 -0700

    update style color and fonts and create 3d text

--- src/components/PortfolioCube/PortfolioCube.js
+++ src/components/PortfolioCube/PortfolioCube.js
@@ -1,10 +1,10 @@
-
+import Aux from '../../containers/hoc/Auxiliary';
 import classes from './PortfolioCube.module.css';
 
 function PortfolioCube() {
     return (
         <div className={[classes.PortfolioCube, 'font-limelight'].join(' ')}>
-            <h1>Coming Soon...</h1>
+            <h1 className={classes.Title}>Coming Soon...</h1>          
         </div>
     );
 }
